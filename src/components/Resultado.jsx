59fc9fda0b92f28a533c131081d439a3d032a252
Brian <brianfsosa@gmail.com>
2022-07-19 16:35:56 -0300

    finalizando

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,13 +1,39 @@
 import useCotizador from "../hooks/useCotizador"
+import { MARCAS, PLANES } from '../constants/'
 
 function Resultado() {
-    const { resultado } = useCotizador()
-    if (resultado === 0 ) {
-        return null
-    }
-  return (
-    <div>{resultado}</div>
-  )
+    const { resultado, datos } = useCotizador()
+    const { marca, plan, year } = datos
+    
+    const [nombrePlan] = PLANES.filter(p => p.id === Number(plan) )
+    
+    if (resultado === 0 ) return null
+    
+    return (
+        <div className="bg-gray-100 text-center mt-5 p-5 shadow">
+            <h2 className="text-gray-600 font-black text-3xl">
+                Resumen
+            </h2>
+
+            <p className="my-2">
+                <span className="font-bold">Marca: </span>
+                {marca}
+            </p>
+            <p className="my-2">
+                <span className="font-bold">Plan: </span>
+                {nombrePlan.nombre}
+            </p>
+            <p className="my-2">
+                <span className="font-bold">Año del auto: </span>
+                {year}
+            </p>
+            <p className="my-2 text-2xl">
+                <span className="font-bold">Total de cotización: </span>
+                ${resultado}
+            </p>
+            
+        </div>
+    )
 }
 
 export default Resultado
